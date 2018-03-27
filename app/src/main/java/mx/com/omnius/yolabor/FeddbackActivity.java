d9a257fff0a2be8ec1cefea415a596d93ae394f1
Fernando <fercho_fer_12@hotmail.com>
2018-03-27 11:22:12 -0600

    Feedback activity create

---
+++ app/src/main/java/mx/com/omnius/yolabor/FeddbackActivity.java
@@ -0,0 +1,11 @@
+package mx.com.omnius.yolabor;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by omnius on 26/03/18.
+ */
+
+public class FeddbackActivity extends AppCompatActivity {
+
+}
